ae6250b641baec35ba3c1f1ed539d2c9d5e906ae
jigz1986 <jigz1986@gmail.com>
2024-02-26 16:16:44 +0530

    Add files via upload

--- score.js
+++
@@ -1,4 +0,0 @@
-let params = new URLSearchParams(document.location.search);
-let name = params.get("fname"); // is the string "Jonathan"
-let age = parseInt(params.get("age"), 10); // is the number 18
-console.log(name, age);
